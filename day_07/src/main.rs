623bc625f4bff060f1a86a99da173f2d31484fd4
Daniel Escanez-Exposito <jescanez@ull.edu.es>
2024-12-07 19:31:56 +0000

    Day_07.2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::{repeat_n, Itertools};
 use std::fs;
 
 struct CalibrationEquiation {
@@ -16,7 +17,12 @@ impl CalibrationEquiation {
             .split_whitespace()
             .map(|operand| operand.parse::<u64>().unwrap())
             .collect();
-        let operators = vec![|a: u64, b: u64| a + b, |a: u64, b: u64| a * b];
+        let operators = vec![
+            |a: u64, b: u64| a + b,
+            |a: u64, b: u64| a * b,
+            |a: u64, b: u64| (a.to_string() + &b.to_string()).parse::<u64>().unwrap(),
+        ];
+
         CalibrationEquiation {
             result,
             operands,
@@ -24,15 +30,14 @@ impl CalibrationEquiation {
         }
     }
 
-    fn get_operands_value(&self, i: usize) -> Option<u64> {
+    fn eval_combination(&self, operator_combination: Vec<fn(u64, u64) -> u64>) -> Option<u64> {
         self.operands
             .clone()
             .into_iter()
             .skip(1)
             .enumerate()
             .try_fold(self.operands[0], |acc, (j, operand)| {
-                let current_bit = (i >> j) & 1;
-                let current_result = self.operators[current_bit](acc, operand);
+                let current_result = operator_combination[j](acc, operand);
 
                 if current_result <= self.result {
                     Some(current_result)
@@ -44,8 +49,9 @@ impl CalibrationEquiation {
     }
 
     pub fn get_partial_result(&self) -> u64 {
-        (0..2_usize.pow(self.operands.len() as u32 - 1))
-            .find_map(|i| self.get_operands_value(i))
+        repeat_n(self.operators.clone().into_iter(), self.operands.len())
+            .multi_cartesian_product()
+            .find_map(|operator_combination| self.eval_combination(operator_combination))
             .unwrap_or(0)
     }
 }
@@ -102,5 +108,23 @@ mod tests {
     fn no_operations_for_this() {
         let calibration_eq = CalibrationEquiation::new("21037: 9 7 18 13");
         assert_eq!(calibration_eq.get_partial_result(), 0);
+    }
+
+    #[test]
+    fn concatenation() {
+        let calibration_eq = CalibrationEquiation::new("156: 15 6");
+        assert_eq!(calibration_eq.get_partial_result(), 156);
+    }
+
+    #[test]
+    fn concatenation_and_sum() {
+        let calibration_eq = CalibrationEquiation::new("192: 17 8 14");
+        assert_eq!(calibration_eq.get_partial_result(), 192);
+    }
+
+    #[test]
+    fn all_operations() {
+        let calibration_eq = CalibrationEquiation::new("7290: 6 8 6 15");
+        assert_eq!(calibration_eq.get_partial_result(), 7290);
     }
 }
