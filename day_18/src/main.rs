2133fdc3b9c7e386276c916b9b3d42749217cee6
Daniel Escanez-Exposito <jescanez@ull.edu.es>
2024-12-22 17:27:19 +0000

    Day_18.2

--- day_18/src/main.rs
+++ day_18/src/main.rs
@@ -16,9 +16,22 @@ fn main() {
     });
     let ram_string =
         fs::read_to_string(input_filename).expect("Should have been able to read the file");
+    // TODO: Decouple functionality from the number of limiting bytes of the parser. It should be the responsibility of the Ram class.
     let (_, ram) = read_input(&ram_string, 70, 1024).unwrap();
     let result_part1 = get_result_part1(&ram);
     println!("Result part 1: {}", result_part1);
+
+    let mut result_part2 = 0;
+    // TODO: Add binary search
+    for i in 0..=3450 {
+        let (_, ram) = read_input(&ram_string, 70, i).unwrap();
+        let result = get_result_part1(&ram);
+        if result == 0 {
+            result_part2 = i;
+            break;
+        }
+    }
+    println!("Result part 2: {}", result_part2);
 }
 
 #[cfg(test)]
